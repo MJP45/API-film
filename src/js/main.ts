84612faaf23095f9a6e289fd9a821d83cef3469b
Max Jaeckel-Parker <max.jaeckel-parker@nology.io>
2019-10-23 09:03:15 +0100

    initial interaction with the API loading data and images onto the page

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,5 +1,40 @@
 import { Person, sum } from './sums/sum';
+import { cloneNode } from '@babel/types';
 
-const ollie: Person = new Person("Ollie", 56);
-console.log(`Wow, Ollie is only ${ollie.age}`);
-console.log(sum(50, 3));
+const url = `http://www.omdbapi.com/?s=war&apikey=ed344e9d`
+
+export interface IMovies {
+  Title: string,
+  Year: number,
+  Poster: string,
+}
+
+export interface IMoviesResponse {
+  Search: IMovies[],
+}
+
+const buildList = (data: IMoviesResponse) => {
+  const ul = document.getElementsByTagName("ul")[0];
+    data["Search"].forEach((movie) => {
+      let li = document.createElement("li");
+      let h2 = document.createElement("h2");
+      let p = document.createElement("p");
+      let img = document.createElement("img");
+      h2.innerHTML = `${movie.Title}`;
+      p.innerHTML = `${movie.Year}`;
+      img.src = `${movie.Poster}`;
+      li.append(h2, p, img);
+      ul.append(li);
+      console.log(movie);
+    });
+    
+}
+fetch(url)
+.then((response) => response.json())
+.then((data) => buildList(data))
+.catch((error) => {
+    console.log(error);
+});
+
+
+const searchButton = document.getElementById("search-button");
